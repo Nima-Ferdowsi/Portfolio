bda0be39bfbefd0656d41f1230b0f71dc4b80692
Nima Ferdowsi <nimalangariferdowsi1383@gmail.com>
2021-09-18 12:33:25 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
+function dateAgo(startDate, endDate) {
+  var startDate = new Date(startDate);
+  var diffDate = new Date(new Date(endDate) - startDate);
+  return (
+    diffDate.toISOString().slice(0, 4) - 1970 + "Y " + diffDate.getMonth() + "M"
+  );
+}
 function Skills(name, value) {
   this.name = name;
   this.value = value;
 }
-function Experience(name, pos, body) {
+function Experience(name, pos, startDate, endDate, body) {
   this.name = name;
   this.pos = pos;
+  this.date = dateAgo(startDate, endDate);
   this.body = body;
 }
 function Sample(title, link, body) {
@@ -43,24 +51,37 @@ const mySkills = [
 //experience
 const Pyaramid = new Experience(
   "Pyramid.Inv",
-  "Fullstack developer | Turkey | 1y",
+  "Fullstack developer | Ireland ",
+  "2019-01-01",
+   new Date(),
   ` Pyramid.Inv was my first company that I was.
 This company was not public 
 company it was organization between companies for buying stocks.....`
 );
 const CloudTree = new Experience(
   "CloudTree/Cloud4Agri",
-  "Frontend developer | ireland | 4m",
+  "Frontend developer | Ireland ",
+  "2020-12-10",
+  "2021-05-10",
   ` CloudTree was my second company that I was.
    in this company I work on both website(regular) and mobile apps(games). 
   `
 );
 const Freelancer = new Experience(
   "Freelancer",
-  "Freelancer | international | 9m",
+  "Freelancer | international ",
+  "2020-06-01",
+  new Date(),
   ""
 );
-const myExperience = [Pyaramid, CloudTree, Freelancer];
+const NumberLand = new Experience(
+  "Numberland",
+  "Frontend Developer | Iran ",
+  "2020-10-01",
+  new Date(),
+  "In this company I was Responsible for frontend this company it was a company that selling virtual numbers to clients "
+);
+const myExperience = [Pyaramid, CloudTree];
 
 //sample
 const Movie = new Sample(
@@ -77,8 +98,15 @@ const WhatsApp = new Sample(
 JAVASCRIPT , REACT ,Redux, Material-ui , bootstrap backend technology :nodejs , express js ,
 mongodb, monggose`
 );
+const Ecommerce = new Sample(
+  "Ecommerce",
+  "https://rise-ecommerce.netlify.app/",
+  `This site is fullstack (mern stack) ecommerce project 
+  frontend: react and redux, bootstrap
+  backend: node js,express ,mongodb,`
+);
 
-const mySamples = [Movie, WhatsApp];
+const mySamples = [Movie, WhatsApp, Ecommerce];
 
 const addProgressBar = () => {
   for (let i = 0; i < mySkills.length; i++) {
@@ -106,7 +134,7 @@ const addExperience = () => {
                 ${element.name}
             </h5>
             <h5 class="resume_item_pos">
-            ${element.pos}
+            ${element.pos}|${element.date}
            </h5>
   
 
@@ -149,5 +177,5 @@ const addSamples = () => {
 document.addEventListener("DOMContentLoaded", () => {
   addProgressBar();
   addExperience();
-  addSamples();
-});
+/*   addSamples();
+ */});
